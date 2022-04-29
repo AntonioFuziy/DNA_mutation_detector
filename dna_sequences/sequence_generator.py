f81be9ec2fe94028b387b67d25a5620b14e7b8f7
AntonioFuziy <antoniovf@al.insper.edu.br>
2022-04-28 22:03:12 -0300

    feat: added sequence generator

---
+++ dna_sequences/sequence_generator.py
@@ -0,0 +1,19 @@
+import random
+
+max_sequence_length = 300
+filename = 1
+
+for n in range(1, max_sequence_length + 1, 1):
+  m = random.randint(n, n*4)
+
+  file = f"./all_sequences/dna{filename}.seq"
+  f = open(file, 'w')
+  seq=[str(n)+'\n',
+      str(m)+'\n',
+      ''.join(random.choices(['A','T','C','G','-'],k=n))+'\n',
+      ''.join(random.choices(['A','T','C','G','-'],k=m))]
+  f.writelines(seq)
+  f.close()
+  filename += 1
+
+# print(''.join(seq))
