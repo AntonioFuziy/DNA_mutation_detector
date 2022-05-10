cd50e282971b3b55ac35eca8b65958bfb1d301af
AntonioFuziy <antoniovf@al.insper.edu.br>
2022-05-10 20:35:55 -0300

    feat: a try for parallel local search

---
+++ parallel/busca-local/busca_local_parallel.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<random>
+#include<fstream>
+#include<omp.h>
+
+using namespace std;
+
+int match(char a, char b){
+  if(a == b){
+    return 2;
+  }
+  return -1;
+}
+
+string generate_subsequence(string seq, int k, int n_m){
+  string sequence_generated;
+  random_device rd;
+  unsigned seed = rd();
+  default_random_engine generator(seed);
+  uniform_int_distribution<int> distribution(0, n_m-k);
+
+  int index = distribution(generator);
+  if(index > int(seq.size())){
+    index = seq.size();
+  }
+
+  while(int(sequence_generated.size()) < k){
+    sequence_generated += seq[index];
+    index++;
+  }
+
+  return sequence_generated;
+}
+
+int calculate_score(string a, string b){
+  int score = 0;
+  for (int i = 0; i < int(a.size()); i++){
+    score += match(a[i], b[i]);
+  }
+
+  return score;
+}
+
+int main(){
+  int n, m;
+  string a, b;
+
+  cin >> n;
+  cin >> m;
+
+  cin >> a;
+  cin >> b;
+
+  if(n > m){
+    string change_a = a;
+    a = b;
+    b = change_a;
+    int change_n = n;
+    n = m;
+    m = change_n; 
+  }
+
+  random_device rd;
+  unsigned seed = rd();
+  default_random_engine generator(seed);
+  uniform_int_distribution<int> distribution(1, n);
+  int k = distribution(generator);
+
+  string subsequence_a = generate_subsequence(a, k, n);
+
+  int p = distribution(generator);
+
+  vector<string> subsequences_b;
+  int best_sequence_index = 0;
+  int max_score = 0;
+
+  omp_set_num_threads(4);
+
+  #pragma omp parallel for shared(subsequences_b, subsequence_a, max_score)
+  for(int i = 0; i < p; i++){
+    string subsequence_b = generate_subsequence(b, k, m);
+    subsequences_b.push_back(subsequence_b);
+    int current_score = calculate_score(subsequence_a, subsequence_b);
+
+    #pragma omp critical
+    {
+      if(current_score > max_score){
+        max_score = current_score;
+        best_sequence_index = i;
+      }
+    }
+  }
+
+  cout << "" << endl;
+  cout << "Score: " << max_score << endl;
+
+  cout << "" << endl;
+  cout << "Sequence A: " << subsequence_a << endl;
+  cout << "Sequence B: " << subsequences_b[best_sequence_index] << endl;
+
+  return 0;
+}
--- parallel/busca-local/busca_local_sequential.cpp
+++ parallel/busca-local/busca_local_sequential.cpp
@@ -3,32 +3,55 @@
 #include<vector>
 #include<random>
 #include<fstream>
-#include<omp.h>
 
 using namespace std;
 
 int match(char a, char b){
   if(a == b){
     return 2;
-  }
+  };
   return -1;
 }
 
-string generate_subsequence(string seq, int k, int n_m){
+string generate_subsequence_a(string seq_a, int k, int n){
   string sequence_generated;
   random_device rd;
   unsigned seed = rd();
   default_random_engine generator(seed);
-  uniform_int_distribution<int> distribution(0, n_m-k);
+  uniform_int_distribution<int> distribution(0, n-k);
 
-  int index = distribution(generator);
-  if(index > int(seq.size())){
-    index = seq.size();
+  int index_a = distribution(generator);
+  if(index_a > int(seq_a.size())){
+    index_a = seq_a.size();
   }
 
+  // cout << "Random index for subsequence A: " << index_a << endl;
+
   while(int(sequence_generated.size()) < k){
-    sequence_generated += seq[index];
-    index++;
+    sequence_generated += seq_a[index_a];
+    index_a++;
+  }
+
+  return sequence_generated;
+}
+
+string generate_subsequence_b(string seq_b, int k, int m){
+  string sequence_generated;
+  random_device rd;
+  unsigned seed = rd();
+  default_random_engine generator(seed);
+  uniform_int_distribution<int> distribution(0, m-k);
+
+  int index_b = distribution(generator);
+  if(index_b > int(seq_b.size())){
+    index_b = seq_b.size();
+  }
+
+  // cout << "Random index for subsequence B: " << index_b << endl;
+
+  while(int(sequence_generated.size()) < k){
+    sequence_generated += seq_b[index_b];
+    index_b++;
   }
 
   return sequence_generated;
@@ -68,7 +91,7 @@ int main(){
   uniform_int_distribution<int> distribution(1, n);
   int k = distribution(generator);
 
-  string subsequence_a = generate_subsequence(a, k, n);
+  string subsequence_a = generate_subsequence_a(a, k, n);
 
   int p = distribution(generator);
 
@@ -77,16 +100,12 @@ int main(){
   int max_score = 0;
 
   for(int i = 0; i < p; i++){
-    string subsequence_b = generate_subsequence(b, k, m);
+    string subsequence_b = generate_subsequence_b(b, k, m);
     subsequences_b.push_back(subsequence_b);
     int current_score = calculate_score(subsequence_a, subsequence_b);
-
-    #pragma omp critical
-    {
-      if(current_score > max_score){
-          max_score = current_score;
-          best_sequence_index = i;
-      }
+    if(current_score > max_score){
+      max_score = current_score;
+      best_sequence_index = i;
     }
   }
 
